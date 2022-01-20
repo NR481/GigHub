e4126d9d83a784bb23b15ce30688b4d7e14ccd64
Nick Rogers <nickjrogers@gmail.com>
2022-01-19 16:56:02 -0800

    main page layout

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -41,9 +41,9 @@ function App() {
         <ProtectedRoute path='/users' exact={true} >
           <UsersList/>
         </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
+        {/* <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
-        </ProtectedRoute>
+        </ProtectedRoute> */}
         {/* <ProtectedRoute path='/' exact={true} >
           <h1>My Home Page</h1>
         </ProtectedRoute> */}
