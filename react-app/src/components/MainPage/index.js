eabfbdc0e705ce414677ef57c3292e17a59614cf
Nick Rogers <nickjrogers@gmail.com>
2022-01-20 15:55:50 -0800

    starting comments feature

--- react-app/src/components/MainPage/index.js
+++ react-app/src/components/MainPage/index.js
@@ -45,6 +45,7 @@ const MainPage = () => {
     <div>
       <img
         src='https://images.unsplash.com/photo-1573006939324-641d31296356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+        alt='music'
         className='splash-img'
       />
       <h2>Featured Artists</h2>
