57bc0ba9d641d3c16ab519717077fe6f27fbc64b
Nick Rogers <nickjrogers@gmail.com>
2022-01-19 16:48:12 -0800

    setting up splash page

--- react-app/src/components/MainPage/MainPage.js
+++ react-app/src/components/MainPage/MainPage.js
@@ -1,6 +1,11 @@
+import './MainPage.css'
+
 const MainPage = () => {
   return (
-    <h1>GigHub</h1>
+    <img
+      src='https://images.unsplash.com/photo-1573006939324-641d31296356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+      className='splash-img'
+    />
   )
 }
 
