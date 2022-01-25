6d84bf1707b08eef6fb17648e68ed571cf6c0b46
Nick Rogers <nickjrogers@gmail.com>
2022-01-24 16:52:07 -0800

    debugging ratings

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -16,7 +16,7 @@ const ProfilePage = () => {
   const profile = profileObj[id]
 
   const [showForm, setShowForm] = useState(false)
-  const [profileRating, setProfileRating] = useState(0)
+  const [profileRating, setProfileRating] = useState(-1)
 
   useEffect(() => {
     dispatch(getFeaturedProfiles())
